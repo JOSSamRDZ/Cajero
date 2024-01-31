d578f58e97a374822fad71b63b7752375deccc14
Sam <loboabel.cronox@gmail.com>
2024-01-31 02:59:34 -0600

    Al modulo 3 seleccion de servicio se le agrego el submodulo 3.1 la opcion de retiro, ahora el cliente puede retirar dinero del cajero

--- cajeroB.py
+++ cajeroB.py
@@ -12,16 +12,19 @@ if card_account == card_verification:
         if nip_user == nip_verification:
             print("NIP correcto")
             fondos = 14000
-            retiro = float(input("Ingrese la cantidad que desea retirar: "))
-            # Módulo 3 (Verificación de fondos y finalización del programa)
-            if retiro <= fondos:  # Modificado para incluir el caso de retiro igual a los fondos
-                fondos = fondos - retiro
-                print(f"Retiro: {retiro} MX \n Saldo anterior: {fondos + retiro}\n Saldo actual: {fondos}")
-                print("Operación exitosa, ATM servicios le desea un excelente día")
-                break
-            else:
-                print("No hay fondos suficientes.")
-                break
+             # Módulo 3 (Seleccion de servicio contiene el submodilo 3.1, 3.2, 3.3 y 3.4)
+            user_select=input("RETIRO\nTRANSFERENCIA\nCONSULTA\nDEPOSITO\nEscribe una opcion: ")
+            if user_select == "RETIRO":
+                retiro_user=int(input("Ingresa la catindad que desea retirar: "))
+                if retiro_user <= fondos:
+                    fondos=fondos-retiro_user
+                    print(f"Retiro: {retiro_user} MX \n Saldo anterior: {fondos + retiro_user}\n Saldo actual: {fondos}")
+                    print("Operación exitosa, ATM servicios le desea un excelente día")
+                    break
+                else:
+                    print("No hay fondos suficientes")
+                    break
+                         
         else:
             intentos_nip += 1
             if intentos_nip < 3:
@@ -31,3 +34,12 @@ if card_account == card_verification:
                 break
 else:
     print("Lo siento Cuenta incorrecta")
+    # retiro = float(input("Ingrese la cantidad que desea retirar: "))
+ # if retiro <= fondos:  # Modificado para incluir el caso de retiro igual a los fondos
+            #     fondos = fondos - retiro
+            #     print(f"Retiro: {retiro} MX \n Saldo anterior: {fondos + retiro}\n Saldo actual: {fondos}")
+            #     print("Operación exitosa, ATM servicios le desea un excelente día")
+            #     break
+            # else:
+            #     print("No hay fondos suficientes.")
+            #     break
